e519502f6d78d94795f7f000c4a9c6d1f2903900
CarolineMano <carolinemano@protonmail.ch>
2021-08-16 14:45:06 -0300

    Create Cálculo de média escolar 2 project

---
+++ 1 - Iniciando Aritmética em C#/Cálculo de média escolar 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cálculo_de_média_escolar_2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+
+            double a, b, c, mediaP;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            mediaP = (a * 0.2) + (b * 0.3) + (c * 0.5);
+
+            Console.WriteLine("MEDIA = {0}", mediaP.ToString("0.0"));
+
+        }
+    }
+}
