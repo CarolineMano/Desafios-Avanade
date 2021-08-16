43d54a33795f32ba86e7c984bdbf0b0f31d461d6
CarolineMano <carolinemano@protonmail.ch>
2021-08-16 14:31:16 -0300

    Create Program.cs

---
+++ 1 - Iniciando Aritmética em C#/Cálculo de média escolar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Cálculo_de_média_escolar
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+
+            double a, b, mediaP;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            mediaP = ((a* 3.5) + (b* 7.5)) / (3.5 + 7.5);
+
+            Console.WriteLine("MEDIA = {0}", mediaP.ToString("0.00000"));
+
+        }
+    }
+}
