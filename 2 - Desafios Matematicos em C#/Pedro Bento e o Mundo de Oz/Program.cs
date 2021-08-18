715c9dde2fb2759571eac29e71b20e12dc3db463
CarolineMano <carolinemano@protonmail.ch>
2021-08-18 14:41:10 -0300

    Create Pedro Bento e o Mundo de Oz project

---
+++ 2 - Desafios Matematicos em C#/Pedro Bento e o Mundo de Oz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pedro_Bento_e_o_Mundo_de_Oz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tipoJoia;
+            bool testeTipo = false;
+            int tamanhoMaximo = 0;
+            List<string> tesouro = new List<string>();
+
+            do
+            {
+                tipoJoia = Console.ReadLine();
+                if(!string.IsNullOrEmpty(tipoJoia))
+                {
+                    tamanhoMaximo += tipoJoia.Length;
+                    if(tesouro.Count == 0)
+                        testeTipo = true;
+
+                    else 
+                    {
+                        foreach(var item in tesouro)
+                        {
+                            if(item == tipoJoia)
+                            {
+                                testeTipo = false;
+                                break;
+                            }     
+                        }
+                    }  
+
+                    if (testeTipo)
+                    {
+                        tesouro.Add(tipoJoia);
+                    }
+                    testeTipo = true;
+                }
+            } while (!string.IsNullOrEmpty(tipoJoia) && tamanhoMaximo < 107);
+            Console.WriteLine(tesouro.Count);
+        }
+    }
+}
